aac4f5aadc05a5ba2f84be3958a1e2d531821588
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-10-03 20:56:09 +0100

    corrected def initialize parameter

--- specs/ecosystem_fish_specs.rb
+++ specs/ecosystem_fish_specs.rb
@@ -6,7 +6,7 @@ require ("minitest/rg")
 class TestFish < MiniTest::Test
 
   def setup
-    @da_fish=Fish.new("Tuna Turner")
+    @da_fish = Fish.new("Tuna Turner")
   end
 
   def test_get_da_fish_name
