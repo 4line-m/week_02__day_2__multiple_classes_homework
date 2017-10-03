74bafd3ff5c02ebdb4bcd077affde9177df1c0e1
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-10-03 20:35:05 +0100

    passed da_fish_name test

---
+++ specs/ecosystem_fish_specs.rb
@@ -0,0 +1,19 @@
+require_relative("../ecosystem_fish.rb")
+require("minitest/autorun")
+require ("minitest/rg")
+
+
+class TestFish < MiniTest::Test
+
+  def setup
+    @da_fish=Fish.new("Tuna Turner")
+  end
+
+  def test_get_da_fish_name
+  assert_equal("Tuna Turner", @da_fish.name)
+  end
+
+
+
+
+end
